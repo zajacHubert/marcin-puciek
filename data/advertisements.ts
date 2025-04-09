3ee362c1b069b61a1c55e62721f4c906f010c850
Hubert <hubert.zajac92@gmail.com>
2025-04-09 18:23:57 +0200

    fix videos

--- data/advertisements.ts
+++ data/advertisements.ts
@@ -1,65 +1,22 @@
 export const advertisements: { id: string }[] = [
-  {
-    id: 'OBwTsFm0t5Y',
-  },
-  {
-    id: '28qqi3pqDsU',
-  },
-  {
-    id: 'SuFQtC15Grw',
-  },
-  {
-    id: '9luz2enTArI',
-  },
-  {
-    id: '98_RJWyUXwA',
-  },
-  {
-    id: 'gAeFIKR8SZ4',
-  },
-  {
-    id: 'jgbvBNvHk_4',
-  },
-  {
-    id: 'tzArFcSjupQ',
-  },
-  {
-    id: 'qBfyEiZo67Q',
-  },
-  {
-    id: 'SuFQtC15Grw',
-  },
-  {
-    id: 'JDUnmqNiPis',
-  },
-  {
-    id: '0OFmAxZ8d_o',
-  },
-  {
-    id: 'flOuU4-mLt4',
-  },
-  {
-    id: 'nUONEovB7DQ',
-  },
-  {
-    id: 'M2qSovNxetg',
-  },
-  {
-    id: 'l4i0iLaV9T0',
-  },
-  {
-    id: 'br5fSmGxioM',
-  },
-  {
-    id: 'Z6qDeXbYa-U',
-  },
-  {
-    id: 'k9Idqdzfnho',
-  },
-  {
-    id: '5DL1_xevYfI',
-  },
-  {
-    id: '6xNe3_DbWnU',
-  },
+  { id: 'OBwTsFm0t5Y' },
+  { id: '28qqi3pqDsU' },
+  { id: 'SuFQtC15Grw' },
+  { id: '9luz2enTArI' },
+  { id: '98_RJWyUXwA' },
+  { id: 'gAeFIKR8SZ4' },
+  { id: 'jgbvBNvHk_4' },
+  { id: 'tzArFcSjupQ' },
+  { id: 'qBfyEiZo67Q' },
+  { id: 'JDUnmqNiPis' },
+  { id: '0OFmAxZ8d_o' },
+  { id: 'flOuU4-mLt4' },
+  { id: 'nUONEovB7DQ' },
+  { id: 'M2qSovNxetg' },
+  { id: 'l4i0iLaV9T0' },
+  { id: 'br5fSmGxioM' },
+  { id: 'Z6qDeXbYa-U' },
+  { id: 'k9Idqdzfnho' },
+  { id: '5DL1_xevYfI' },
+  { id: '6xNe3_DbWnU' },
 ];
