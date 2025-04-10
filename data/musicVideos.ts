538a83ed29c7727453d17e0d22dce8c40c760559
Hubert <hubert.zajac92@gmail.com>
2025-04-10 20:47:23 +0200

    add music videos

--- data/musicVideos.ts
+++ data/musicVideos.ts
@@ -1,21 +1,36 @@
 export const musicVideos: { id: string }[] = [
   {
-    id: 'bM4xWww5yag',
+    id: 'q6idj8bMeCc',
   },
   {
-    id: 'q6idj8bMeCc',
+    id: 'XGJNWzXgG_o',
+  },
+  {
+    id: '8b-Urc3UYK0',
   },
   {
     id: '8gXlJHcPfmM',
   },
   {
-    id: 'eHNIrJOZXhA',
+    id: 'WruRJAfU2kI',
+  },
+  {
+    id: 'QiG3guKDDQM',
   },
   {
     id: '_-FnldV5BFU',
   },
   {
-    id: 'FhgesmGUvfk',
+    id: '9kBhlaLh9kw',
+  },
+  {
+    id: 'E7cbeT3hWRY',
+  },
+  {
+    id: 'PiqTEcZSChU',
+  },
+  {
+    id: 'r-uuoDhk-KA',
   },
   {
     id: 'hOFnYZ_0Pco',
@@ -24,6 +39,150 @@ export const musicVideos: { id: string }[] = [
     id: 't4byHlsKpps',
   },
   {
+    id: 'bHXaYnB2dWg',
+  },
+  {
+    id: 'FhgesmGUvfk',
+  },
+  {
+    id: '3HLikU9BQ-Y',
+  },
+  {
+    id: 'eHNIrJOZXhA',
+  },
+  {
+    id: 'tfxDWFdZXuw',
+  },
+  {
+    id: 'Bay3mUqoyBs',
+  },
+  {
+    id: 'gvHymWefdZA',
+  },
+  {
+    id: 'pf0BzzzZp-c',
+  },
+  {
+    id: 'mmkftfVnZS4',
+  },
+  {
+    id: '5udDjYpXK50',
+  },
+  {
+    id: 'c7jRzkY6V3A',
+  },
+  {
+    id: 'ixir9JicRuY',
+  },
+  {
+    id: 'gmn7UNHaPrQ',
+  },
+  {
     id: 'rCyd3ZJCP38',
+  },
+  {
+    id: '30cPxrEunj8',
+  },
+  {
+    id: 'jHJSf2FI-hA',
+  },
+  {
+    id: 'Kw_2aChv7aQ',
+  },
+  {
+    id: 'trq32GidwqA',
+  },
+  {
+    id: 'odEfu3PbJBQ',
+  },
+  {
+    id: 'EGWLT7KELiM',
+  },
+  {
+    id: 'IYqR9fGmQ0Q',
+  },
+  {
+    id: 'kR9oGG1VU90',
+  },
+  {
+    id: 'tWibggCk4ug',
+  },
+  {
+    id: '_rI25eh3G38',
+  },
+  {
+    id: 'j3lFCkcZSHE',
+  },
+  {
+    id: 'Qrekca2tE3Y',
+  },
+  {
+    id: 'D9inh6JVkSo',
+  },
+  {
+    id: '2_V0tgsDS4I',
+  },
+  {
+    id: 'Wh2LEgfFDm4',
+  },
+  {
+    id: 'NwMsyMuCOhw',
+  },
+  {
+    id: 'M-bav75ITVw',
+  },
+  {
+    id: 'nASUT9C1lo4',
+  },
+  {
+    id: 'iiK1_jzydVQ',
+  },
+  {
+    id: '1DpamTxPb6Y',
+  },
+  {
+    id: '5G0WNbHn7ws',
+  },
+  {
+    id: 'hGXEx_bC-Xo',
+  },
+  {
+    id: 'BwP1xF3o1wI',
+  },
+  {
+    id: 'wn2F-CR0ibE',
+  },
+  {
+    id: 'G9WqCxKoRII',
+  },
+  {
+    id: 'Fz29LqUKZDY',
+  },
+  {
+    id: '1PrEC2xWUkE',
+  },
+  {
+    id: 'lDBdVdAiYrY',
+  },
+  {
+    id: 'mj0roNJgbwc',
+  },
+  {
+    id: '06-IhVSP6gQ',
+  },
+  {
+    id: 'z_RRADn53o0',
+  },
+  {
+    id: 'XFiWtrH4Cv0',
+  },
+  {
+    id: '6C_OT8K2eEw',
+  },
+  {
+    id: '1TwWTEEksgg',
+  },
+  {
+    id: '55X0fKXs09I',
   },
 ];
