2a2ffc3109edbd605c964aa4cb61df64aa7c62c4
Hubert <hubert.zajac92@gmail.com>
2025-05-17 14:39:42 +0200

    add optionl chaining for search params

--- app/video/spoty/page.tsx
+++ app/video/spoty/page.tsx
@@ -5,7 +5,7 @@ import { advertisements } from '@/data/advertisements';
 // import { customAdvertisements } from '@/data/customAdvertisements';
 import styles from '@/styles/video-page.module.css';
 
-interface SearchParams {
+interface PageProps {
   searchParams?: {
     page?: string;
   };
@@ -13,20 +13,18 @@ interface SearchParams {
 
 const ITEMS_PER_PAGE = 10;
 
-const AdvertisementsPage = ({ searchParams }: SearchParams) => {
-  const page = parseInt(searchParams?.page ?? '1', 10);
+const AdvertisementsPage = ({ searchParams }: PageProps) => {
+  const page = Math.max(1, parseInt(searchParams?.page ?? '1', 10));
   const totalPages = Math.ceil(advertisements.length / ITEMS_PER_PAGE);
+
   const startIndex = (page - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
-
   const videos = advertisements.slice(startIndex, endIndex);
+
   return (
     <>
       <section className={styles.videoSection}>
         <h2 className={styles.title}>Reklamy</h2>
-        {/* {customAdvertisements.map((video) => (
-          <VideoPlayer src={video.src} key={video.src} />
-        ))} */}
         <div className={styles.container}>
           {videos.map((advertisement) => (
             <div key={advertisement.id} className={styles.videoBox}>
